4a741b675435c6498afdb8f37d38db1dbc70f5e2
Mayank Kandpal <mayankk4@gmail.com>
2016-12-27 17:47:23 +0530

    Moving all code within src/

--- src/image-generator/pil-key-image-generator.py
+++ src/image-generator/pil-key-image-generator.py
@@ -12,9 +12,9 @@ https://pillow.readthedocs.io/en/3.4.x/reference/ImageFont.html
 
 '''
 
-#TODO: Take a proper KV pair as input and generate all the necessary images
-#TODO: Add flags support.
-#TODO: Give a slight gradiant in the background image.
+# TODO: Take a proper KV pair as input and generate all the necessary images
+# TODO: Add flags support.
+# TODO: Give a slight gradiant in the background image.
 
 import PIL
 from PIL import ImageFont
@@ -27,7 +27,7 @@ MAX_W, MAX_H = 2880, 1800
 
 # TODO: Create a common util for key-image and value-image generators.
 # TODO: Choose randomly from an array of chosen colours.
-BG_COLOR = (255,147,41)
+BG_COLOR = (255, 147, 41)
 
 SHADOW_COLOR = "black"
 TEXT_COLOR = "white"
@@ -37,7 +37,7 @@ FONT_SIZE = 250
 SHADOW_WIDTH = 2
 
 # Create an image with a bg colour.
-img=Image.new("RGBA", (MAX_W, MAX_H), BG_COLOR)
+img = Image.new("RGBA", (MAX_W, MAX_H), BG_COLOR)
 draw = ImageDraw.Draw(img)
 
 # Now add text to the image.
@@ -55,13 +55,13 @@ x = (MAX_W - w) / 2
 y = (MAX_H - h) / 2
 
 # Adding shadows first.
-draw.text((x-SHADOW_WIDTH, y), text, font=font, fill=SHADOW_COLOR)
-draw.text((x+SHADOW_WIDTH, y), text, font=font, fill=SHADOW_COLOR)
-draw.text((x, y-SHADOW_WIDTH), text, font=font, fill=SHADOW_COLOR)
-draw.text((x, y+SHADOW_WIDTH), text, font=font, fill=SHADOW_COLOR)
+draw.text((x - SHADOW_WIDTH, y), text, font=font, fill=SHADOW_COLOR)
+draw.text((x + SHADOW_WIDTH, y), text, font=font, fill=SHADOW_COLOR)
+draw.text((x, y - SHADOW_WIDTH), text, font=font, fill=SHADOW_COLOR)
+draw.text((x, y + SHADOW_WIDTH), text, font=font, fill=SHADOW_COLOR)
 
 # Adding text in white.
-draw.text((x,y), text, fill=TEXT_COLOR, font=font)
+draw.text((x, y), text, fill=TEXT_COLOR, font=font)
 
 # img.save("key.png")
 img.save("./test-data/image/key.png")
--- src/image-generator/pil-value-image-generator.py
+++ src/image-generator/pil-value-image-generator.py
@@ -12,7 +12,7 @@ https://pillow.readthedocs.io/en/3.4.x/reference/ImageFont.html
 
 '''
 
-#TODO: Add support for splitting values over multiple images. Otherwise
+# TODO: Add support for splitting values over multiple images. Otherwise
 #      we need to reject images larger than a given size.
 
 
@@ -25,26 +25,29 @@ import textwrap
 
 MAX_W, MAX_H = 2880, 1800
 
-BG_COLOR = (255,147,41)
+BG_COLOR = (255, 147, 41)
 
 # SHADOW_COLOR = "black"
 TEXT_COLOR = "black"
 
 # Create an image with a bg colour.
-img=Image.new("RGBA", (MAX_W, MAX_H), BG_COLOR)
+img = Image.new("RGBA", (MAX_W, MAX_H), BG_COLOR)
 draw = ImageDraw.Draw(img)
 
 # Now add text to the image.
-text = '''Empressite is a mineral form of silver telluride, AgTe. It is a rare, grey, orthorhombic mineral with which can form compact masses, rarely as bipyrimidal crystals.'''
+text = '''Empressite is a mineral form of silver telluride, AgTe. It is a rare,
+grey, orthorhombic mineral with which can form compact masses, rarely as
+bipyrimidal crystals.'''
+
 # Adding paragraph support
-para = textwrap.wrap(text, width=40) # It seems that the width is the number of characters.
+para = textwrap.wrap(text, width=40)  # width = number of characters.
 
 
-font = ImageFont.truetype("Georgia.ttf", 100)
+font = ImageFont.truetype("Georgia.ttf", 100)  # fontsize
 
 # Determining the height at which we want to start the text.
 w_first, h_first = draw.textsize(para[0], font=font)
-current_h = (MAX_H/2) - ((len(para)/2) * h_first)
+current_h = (MAX_H / 2) - ((len(para) / 2) * h_first)
 
 pad = 10
 for line in para:
@@ -58,8 +61,8 @@ for line in para:
     # draw.text((x, y-2), line, font=font, fill=SHADOW_COLOR)
     # draw.text((x, y+2), line, font=font, fill=SHADOW_COLOR)
 
-	# Add test
-    draw.text((x,y), line, fill=TEXT_COLOR, font=font)
+    # Add test
+    draw.text((x, y), line, fill=TEXT_COLOR, font=font)
 
     current_h += h + pad
 
