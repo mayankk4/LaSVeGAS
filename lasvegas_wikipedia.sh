3722723df5aa5a29cad55101465ff043205525df
cycL0 <mayankk4@gmail.com>
2017-09-25 03:35:22 +0530

    Fixing the wikipedia script to reflect the latest updates to the backend.

--- lasvegas_wikipedia.sh
+++ lasvegas_wikipedia.sh
@@ -4,36 +4,29 @@
 ## time sh lasvegas_wikipedia.sh ##
 ###################################
 
-#TODO: FIX this script and the flow.
-
 # Run this script from ./lasvegas
-echo "Running lasvegas_tool."
+echo "Running lasvegas_tool for wikipedia."
 
+
+################################################################################
+################################################################################
+# IMPORTANT : PLEASE SPECIFY A SEPARATE path_to_output FOR EACH WORKER.
+################################################################################
+################################################################################
 
 # clear folders except input.
 rm -R ./test-data/*
-mkdir ./test-data/audio/
-mkdir ./test-data/image/
-mkdir ./test-data/video/
 # Activate virtual env
 echo "Activating virtual environment."
 source venv/bin/activate
 
-echo '{}' > ./src/utils/content/video_status
+echo '{}' > ./src/channels/wikipedia/contentgenerator/video_status
 
 python ./src/channels/wikipedia/main.py \
-  --debug=False \
-  --debug_key="Empressite" \
-  --debug_value="Empressite is a mineral form of silver telluride, AgTe. It is a rare, grey, orthorhombic mineral with which can form compact masses, rarely as bipyrimidal crystals." \
   --path_to_bgcolors_file="./src/utils/bgcolor/modern_colors.txt" \
-  --path_to_key_image="./test-data/image/key.png" \
-  --path_to_value_image="./test-data/image/value.png" \
-  --path_to_key_audio="./test-data/audio/key.mp3" \
-  --path_to_value_audio="./test-data/audio/value.mp3" \
-  --path_to_output="./test-data/video/final_output.mp4" \
-  --video_privacy_status="public" \
-  --path_to_content="./src/utils/content/video_contents"\
-  --path_to_status_file="./src/utils/content/video_status"
+  --path_to_output="./test-data" \
+  --path_to_content="./src/channels/wikipedia/contentgenerator/video_contents"\
+  --path_to_status_file="./src/channels/wikipedia/contentgenerator/video_status"
 
 
 # deactivate virtual env
--- src/lasvegas_wikipedia.py
+++ src/lasvegas_wikipedia.py
@@ -9,11 +9,10 @@ from random import randint
 from oauth2client.tools import argparser
 
 # local libraries
-from imagegenerator import *
-from audiogenerator import *
-from videogenerator import *
-from videouploader import *
-from pipelineexecutor import *
+from lasvegas_state import *
+from lasvegas_worker import *
+
+from wikipedia/executor import *
 
 # List of colours which is read into memory from a text file.
 colors = []
@@ -21,27 +20,18 @@ colors = []
 
 def validate_prod_args(args):
     if not args.path_to_bgcolors_file:
-        exit("1: Please specify a valid file using --path_to_bgcolors_file.")
+        exit("Please specify a valid file using --path_to_bgcolors_file.")
     if not os.path.exists(args.path_to_bgcolors_file):
-        exit("2: Please specify a valid file using --path_to_bgcolors_file.")
+        exit("Please specify a valid file using --path_to_bgcolors_file.")
+
+    if not args.path_to_output:
+        exit("Please add --path_to_output.")
 
     if not args.path_to_content:
-        exit("3: Please check path_to_content.")
-    if not args.path_to_key_image:
-        exit("4: Please check path_to_key_image.")
-    if not args.path_to_value_image:
-        exit("5: Please check path_to_value_image.")
-    if not args.path_to_key_audio:
-        exit("6: Please check path_to_key_audio.")
-    if not args.path_to_value_audio:
-        exit("7: Please check path_to_value_audio.")
-    if not args.path_to_output:
-        exit("8: Please check path_to_output.")
-    if not args.video_privacy_status:
-        exit(
-            "9: Please check video_privacy_status: {'public', 'private', 'unlisted'} ")
+        exit("Please add path_to_content.")
+
     if not args.path_to_status_file:
-        exit("10: Please check path_to_status_file.")
+        exit("Please add --path_to_status_file.")
 
 
 def read_bgcolors(path_to_bgcolors_file):
@@ -58,12 +48,18 @@ def run_pipeline_prod(args):
     # Validate the args
     validate_prod_args(args)
 
+    # Read the colors file into memory once
+    read_bgcolors(args.path_to_bgcolors_file)
+    print "Read " + str(len(colors)) + " colors for background."
+
     # Load candidate datasets for upload
     key_value_pairs = pip_lifecycle.GetAllKeyValues(args.path_to_content)
 
     # For all the key,value in content file
     for key, value in key_value_pairs.iteritems():
-        print "Running for key: " + key
+        print "================================================================"
+        print "Running for title: " + key
+        print "================================================================"
 
         # Starting the execution with status as RUNNING(1)
         started, message = pip_lifecycle.StartExecution(
@@ -72,57 +68,40 @@ def run_pipeline_prod(args):
             print message
             continue
 
-        # Read the colors file into memory once
-        read_bgcolors(args.path_to_bgcolors_file)
-        print "Read " + str(len(colors)) + " colors for background."
 
         # Choose bgcolour
-        key_bg_color = tuple(
-            map(int, colors[randint(0, len(colors) - 1)].split(',')))
-        value_bg_color = tuple(
+        bg_color = tuple(
             map(int, colors[randint(0, len(colors) - 1)].split(',')))
 
-        # Generate images
-        pil_key_image_generator.GenerateImage(key, args.path_to_key_image,
-                                              key_bg_color)
 
-        pil_value_image_generator.GenerateImage(value, args.path_to_value_image,
-
-                                                value_bg_color)
-        # Generate audio
-        gtts_audio_generator.GenerateAudio(key, value,
-                                           args.path_to_key_audio, args.path_to_value_audio)
-
-        # Mux
-        ffmpeg_av_mux.AvMux(args.path_to_key_image, args.path_to_value_image,
-                            args.path_to_key_audio, args.path_to_value_audio,
-                            args.path_to_output)
-
-        # Upload
-        youtube_utils.UploadVideo(
-            key, value, args.path_to_output, args.video_privacy_status)
+        #TODO: UPDATE THIS TO ADD A LIST OF LINES TO VALUE INSTEAD OF A SINGLE VALUE.
+        state = VideoGenerationState(key, [value,], bg_color, args.audio_accent, args.path_to_output, args.upload_to_youtube)
+        ProcessState(state)
 
         # Ending the execution with status SUCCESS(3)
         pip_lifecycle.EndExecution(key, args.path_to_status_file)
+        
+        #TODO: RUN CLEANUPS HERE - CLEAR THE DIRECTORY.
 
         print "================================================================"
-        print "Run successfully completed for " + key
+        print "Run successfully completed for title: " + key
         print "================================================================"
 
-    print "Finished running the pipeline"
+    print "Finished running the pipeline."
 
 
 if __name__ == '__main__':
     # Define all Flags here
+    argparser.add_argument("--upload_to_youtube", default=False)
+
     argparser.add_argument("--path_to_bgcolors_file")
 
+    # Can use en-us, en-uk or en-au.
+    argparser.add_argument("--audio_accent")
+
     # Debugging related flags
-    argparser.add_argument("--path_to_key_image")
-    argparser.add_argument("--path_to_value_image")
-    argparser.add_argument("--path_to_key_audio")
-    argparser.add_argument("--path_to_value_audio")
     argparser.add_argument("--path_to_output")
-    argparser.add_argument("--video_privacy_status")
+
     argparser.add_argument("--path_to_content")
     argparser.add_argument("--path_to_status_file")
 
