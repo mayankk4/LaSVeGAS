b51fd7a3be02bafc9601d2635143554dbd280132
Mayank Kandpal <mayankk4@gmail.com>
2018-03-17 21:18:36 +0530

    Adding a backup of the description data

--- src/channels/goodreads/goodreads_spider/goodreads_spider/spiders/book_description_scraper.py
+++ src/channels/goodreads/goodreads_spider/goodreads_spider/spiders/book_description_scraper.py
@@ -3,6 +3,23 @@ import scrapy
 import re
 import unicodedata
 
+def remove_text_inside_brackets(text):
+    ret = ''
+    skip1c = 0
+    skip2c = 0
+    for i in text:
+        if i == '[':
+            skip1c += 1
+        elif i == '(':
+            skip2c += 1
+        elif i == ']' and skip1c > 0:
+            skip1c -= 1
+        elif i == ')'and skip2c > 0:
+            skip2c -= 1
+        elif skip1c == 0 and skip2c == 0:
+            ret += i
+    return ret
+
 # Extracts ~52257 book titles and ids from 525 pages
 class BookDescriptionSpider(scrapy.Spider):
     name = "bookdescription"
@@ -25,6 +42,9 @@ class BookDescriptionSpider(scrapy.Spider):
     def parse(self, response):
         title = response.xpath('//h1[@class="bookTitle"]/text()').extract_first().strip()
         title = unicodedata.normalize('NFKD', title).encode('ascii','ignore')
+        title = re.sub('<[^>]+>', ' ', title) # remove <>
+        title = remove_text_inside_brackets(title) # remove () []
+        title = ' '.join(title.split()).strip()
 
         desc_list = response.xpath('//div[@id="description"]/span')
         num_desc = len(desc_list)
@@ -48,12 +68,22 @@ class BookDescriptionSpider(scrapy.Spider):
         desc = unicodedata.normalize('NFKD', desc).encode('ascii','ignore')
         # replace text betewen < > with ' '
         desc = re.sub('<[^>]+>', ' ', desc)
+
+        # Some manual data fixing
+        desc = desc.replace("READING GROUP GUIDE ENCLOSED", "")
+
+
         # replace multiple whitespaces with one
-        desc = ' '.join(desc.split())
-        
-        data = title + ":::::" + desc
+        desc = ' '.join(desc.split()).strip()
+
+        if len(title) < 4:
+            return
+        if len(desc) < 175:
+            return
 
         book_id = str(response.url.split('/')[-1].split('.')[0].split('-')[0])
+        data = book_id + ":::::" + title + ":::::" + desc
+
         filename = 'description/book-%s.txt' % book_id
         with open(filename, 'wb') as f:
             f.write("%s\n" % data)
